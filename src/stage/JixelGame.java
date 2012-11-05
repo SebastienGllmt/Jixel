5072aa2c3e98809803c2c6f46f104f18d7d54bc7
SebastienGllmt <SebastienGllmt@gmail.com>
2012-11-04 23:47:45 -0800

    More graphics taken away from user and pushed to engine

--- src/stage/JixelGame.java
+++ src/stage/JixelGame.java
@@ -1,6 +1,9 @@
 package stage;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 import console.JixelConsole;
 import console.JixelVariableManager;
@@ -18,7 +21,16 @@ public class JixelGame extends Canvas{
 	public int tileSize;
 	
 	private JixelScreen screen;
+	private JFrame frame;
 	public final String GAME_TITLE;
+	
+	private long timer;
+	private long lastTime;
+	private double ns;
+	private double delta;
+	private int frames;
+	private int updates;
+	long now;
 	
 	public JixelGame(String title, int width, int height, int scale, int tileSize){
 		GAME_TITLE = title;
@@ -27,7 +39,53 @@ public class JixelGame extends Canvas{
 		this.scale = scale;
 		this.tileSize = tileSize;
 		
+		Dimension size = new Dimension(width*scale, height*scale);
+		setPreferredSize(size);
+		
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setTitle(title);
+		frame.add(this);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+		vm.newVar("paused", false);
 		screen = new JixelScreen(width, height, tileSize);
+	}
+	
+	public void startTime(double fps){
+		ns = 1000000000.0 / fps;
+		timer = System.currentTimeMillis();
+		lastTime = System.nanoTime();
+		delta = 0;
+		frames = 0;
+		updates = 0;
+		now = System.nanoTime();
+	}
+	public void updateTime(){
+		frames++;
+		
+		if(System.currentTimeMillis() - timer > 1000){
+			timer += 1000;
+			frame.setTitle(GAME_TITLE + " Ups: " + updates + " , Fps: " + frames);
+			updates=0;
+			frames=0;
+		}
+		
+		now = System.nanoTime();
+		delta += (now - lastTime) / ns;
+		lastTime = now;
+	}
+	public boolean timeToUpdate(){
+		if(delta >= 1){
+			updates++;
+			delta--;
+			return true;
+		}else{
+			return false;
+		}
 	}
 	
 	public String getTitle(){
@@ -50,6 +108,9 @@ public class JixelGame extends Canvas{
 	}
 	public static JixelConsole getConsole(){
 		return con;
+	}
+	public boolean getPaused(){
+		return vm.getValue("paused");
 	}
 
 }
