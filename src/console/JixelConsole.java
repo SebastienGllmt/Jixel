c057d6c9f9e17bdd4c1d372b2e19accafc0eee68
SebastienGllmt <SebastienGllmt@gmail.com>
2012-11-12 12:26:26 -0800

    Can now type in console + it will now log messages + better use of threads.

--- src/console/JixelConsole.java
+++ src/console/JixelConsole.java
@@ -1,44 +1,57 @@
 package console;
 
-import java.util.Scanner;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 import stage.JixelGame;
 
 public class JixelConsole implements Runnable {
 
-	private Scanner scan = new Scanner(System.in);
 	private Thread thread;
 	private boolean isRunning = false;
-	private boolean isActive = true;
 	private JixelGame game;
-	
-	public JixelConsole(JixelGame game){
+	private List<String> messageList = new ArrayList<String>();
+	private int logHeight;
+
+	public JixelConsole(JixelGame game) {
 		this.game = game;
 		thread = new Thread(this, "Console");
+		thread.start();
+		this.logHeight = (game.getHeight() - 3 * (game.tileSize)) / 24;
 	}
 
 	public void print(String message) {
-		System.out.println(message);
+		int size = messageList.size();
+		if (size - 1 == logHeight) {
+			messageList.remove(size - 1);
+		}
+		messageList.add(0, message);
+	}
+
+	public List<String> getMessageList() {
+		return messageList;
 	}
 
 	public void cInput(String[] input) {
 		String answer = "Unknown command.";
 
 		if (input.length == 1) {
-			answer = String.valueOf((game.getVM().getValue(input[0])));
-			if(!answer.equals("null")){
-				print(answer);
+			answer = String.valueOf((JixelGame.getVM().getValue(input[0])));
+			if (!answer.equals("null")) {
+				print("Value of " + input[0] + ": " + answer);
 			}
 		} else if (input.length == 2) {
 			answer = "Failed to get";
 			if (input[0].equals("save")) {
-				if (game.getVM().save(input[1])) {
+				if (JixelGame.getVM().save(input[1])) {
 					answer = "Profile saved.";
 				} else {
 					answer = "Save failed.";
 				}
 			} else if (input[0].equals("load")) {
-				if (game.getVM().load(input[1])) {
+				if (JixelGame.getVM().load(input[1])) {
 					answer = "Profile loaded.";
 				} else {
 					answer = "Loading failed.";
@@ -52,72 +65,72 @@ public class JixelConsole implements Runnable {
 			}
 			print(answer);
 		} else if (input.length == 3) {
-			String previousValue = String.valueOf(game.getVM().getValue(input[1]));
-			if (input[0].equals("boolean")) {
-				game.getVM().setValue(input[1], Boolean.parseBoolean(input[2]));
-			} else if (input[0].equals("byte")) {
-				game.getVM().setValue(input[1], Byte.parseByte(input[2]));
-			} else if (input[0].equals("char")) {
-				game.getVM().setValue(input[1], input[2].charAt(0));
-			} else if (input[0].equals("short")) {
-				game.getVM().setValue(input[1], Short.parseShort(input[2]));
-			} else if (input[0].equals("int")) {
-				game.getVM().setValue(input[1], Integer.parseInt(input[2]));
-			} else if (input[0].equals("long")) {
-				game.getVM().setValue(input[1], Long.parseLong(input[2]));
-			} else if (input[0].equals("double")) {
-				game.getVM().setValue(input[1], Double.parseDouble(input[2]));
-			} else if (input[0].equals("float")) {
-				game.getVM().setValue(input[1], Float.parseFloat(input[2]));
-			} else if (input[0].equals("String")) {
-				game.getVM().setValue(input[1], input[2]);
-			}
-			String newValue = String.valueOf(game.getVM().getValue(input[1]));
-			if(newValue.equals(previousValue)){
-				answer = "Failed to set";
-			}else{
-				answer = "Value set";
+			if (JixelGame.getVM().contains(input[1])) {
+				Class<?> clazz = JixelGame.getVM().getValue(input[1]).getClass();
+				if (clazz.equals(String.class)) {
+					JixelGame.getVM().setValue(input[1], input[2]);
+					answer = "Value of " + input[1] + " set to " + input[2];
+				} else {
+					Method[] methods = clazz.getMethods();
+					for (Method method : methods) {
+						if (method.getName().startsWith("parse")) {
+							try {
+								JixelGame.getVM().setValue(input[1], method.invoke(clazz, input[2]));
+								answer = "Value of " + input[1] + " set to " + input[2];
+							} catch (IllegalArgumentException e) {
+								answer = "Illegal Argument Exception: Failed to set value of " + input[1] + " to " + input[2];
+							} catch (IllegalAccessException e) {
+								answer = "Illegal Access Exception: Failed to set value of " + input[1] + " to " + input[2];
+							} catch (InvocationTargetException e) {
+								answer = "Invocation Target Exception: Failed to set value of " + input[1] + " to " + input[2];
+							}
+							break;
+						}
+					}
+				}
+
+			} else {
+				answer = "Value failed to set";
 			}
 			print(answer);
 		}
 		return;
 	}
-	
-	public boolean isRunning(){
+
+	public boolean isRunning() {
 		return isRunning;
 	}
-	
-	public void setState(boolean state){
+
+	public void setState(boolean state) {
 		isRunning = state;
 		if(isRunning){
-			if(!thread.isAlive()){
-				thread.start();
+			synchronized(thread){
+				thread.notifyAll();
 			}
 		}
-	}
-	public void shutdown(){
-		isActive=false;
 	}
 
 	@Override
 	public void run() {
-		while(isActive){
-			synchronized(game){
+		while(true){
+			synchronized(thread){
 				try {
-					game.wait();
+					thread.wait();
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
 			while (isRunning) {
-				/*
-				if (scan.hasNextLine()) {
-					String[] commands = scan.nextLine().split(" ");
-					cInput(commands);
+				if (!game.getInput().isReading()) {
+					String msg = game.getInput().getConsoleMsg();
+					if (!msg.isEmpty()) {
+						String[] commands = msg.split(" ");
+						cInput(commands);
+					}
+					int enterKey = JixelGame.getVM().getValue("Jixel_enterKey");
+					game.getInput().startConsoleMsg(enterKey, 32);
 				}
-				*/
 			}
-			
 		}
 	}
 
