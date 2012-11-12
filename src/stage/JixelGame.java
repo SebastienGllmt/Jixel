3349d162e6b32dedd851dbdcca4222d57f0c707d
SebastienGllmt <SebastienGllmt@gmail.com>
2012-11-12 12:29:54 -0800

    Can now limit fps along with ups

--- src/stage/JixelGame.java
+++ src/stage/JixelGame.java
@@ -13,42 +13,44 @@ import console.JixelVariableManager;
 import gui.JixelScreen;
 
 @SuppressWarnings("serial")
-public class JixelGame extends Canvas{
+public class JixelGame extends Canvas {
 
-	private JixelVariableManager vm;
+	private static JixelVariableManager vm;
 	private JixelConsole con;
-	
+
 	public boolean playing = true;
 	public int width;
 	public int height;
 	public int scale;
 	public int tileSize;
 	public final String GAME_TITLE;
-	
+
 	private JixelScreen screen;
 	private JFrame frame;
 	private BufferStrategy bs;
-	
+
+	private double fpsNS;
+	private double upsNS;
 	private long timer;
 	private long lastTime;
-	private double ns;
-	private double delta;
+	private long now;
+	private double deltaUps;
+	private double deltaFps;
 	private int frames;
 	private int updates;
-	long now;
 	
 	private JixelInput input;
-	
-	public JixelGame(String title, int width, int height, int scale, int tileSize){
+
+	public JixelGame(String title, int width, int height, int scale, int tileSize) {
 		GAME_TITLE = title;
 		this.width = width;
 		this.height = height;
 		this.scale = scale;
 		this.tileSize = tileSize;
-		
-		Dimension size = new Dimension(width*scale, height*scale);
+
+		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
-		
+
 		frame = new JFrame();
 		frame.setResizable(false);
 		frame.setTitle(title);
@@ -57,85 +59,111 @@ public class JixelGame extends Canvas{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-		
+
 		vm = new JixelVariableManager(this);
 		input = new JixelInput(this);
-		
+
 		con = new JixelConsole(this);
-		
+
 		vm.newVar("Jixel_paused", false);
-		 
+
 		screen = new JixelScreen(this, width, height, tileSize);
 		createBufferStrategy(3);
 		bs = getBufferStrategy();
-		
+
 		addKeyListener(input);
 	}
-	
-	public void startTime(double ups){
-		ns = 1000000000.0 / ups;
+
+	public void startTime(double fps, double ups) {
+		fpsNS = 1000000000.0 / fps;
+		upsNS = 1000000000.0 / ups;
+
 		timer = System.currentTimeMillis();
 		lastTime = System.nanoTime();
-		delta = 0;
+
+		deltaUps = 0;
+		deltaFps = 0;
 		frames = 0;
 		updates = 0;
+
 		now = System.nanoTime();
 	}
-	public void updateTime(){
-		frames++;
-		
-		if(System.currentTimeMillis() - timer > 1000){
+
+	public void updateTime() {
+
+		if (System.currentTimeMillis() - timer > 1000) {
 			timer += 1000;
 			frame.setTitle(GAME_TITLE + " Ups: " + updates + " , Fps: " + frames);
-			updates=0;
-			frames=0;
+			updates = 0;
+			frames = 0;
 		}
-		
+
 		now = System.nanoTime();
-		delta += (now - lastTime) / ns;
+		deltaFps += (now - lastTime) / fpsNS;
+		deltaUps += (now - lastTime) / upsNS;
 		lastTime = now;
 	}
-	public boolean timeToUpdate(){
-		if(delta >= 1){
-			updates++;
-			delta--;
+
+	public boolean timeForFrame() {
+		if (deltaFps >= 1) {
+			frames++;
+			deltaFps--;
 			return true;
-		}else{
+		} else {
 			return false;
 		}
 	}
-	
-	public String getTitle(){
+	public boolean timeForUpdate() {
+		if (deltaUps >= 1) {
+			updates++;
+			deltaUps--;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public String getTitle() {
 		return GAME_TITLE;
 	}
-	public int getWidth(){
+
+	public int getWidth() {
 		return width;
 	}
-	public int getHeight(){
+
+	public int getHeight() {
 		return height;
 	}
-	public int getScale(){
+
+	public int getScale() {
 		return scale;
 	}
-	public int getTileSize(){
+
+	public int getTileSize() {
 		return tileSize;
 	}
-	public synchronized JixelScreen getScreen(){
+
+	public synchronized JixelScreen getScreen() {
 		return screen;
 	}
-	public synchronized JixelConsole getConsole(){
+
+	public synchronized JixelConsole getConsole() {
 		return con;
 	}
-	public synchronized JixelVariableManager getVM(){
+
+	public static synchronized JixelVariableManager getVM() {
 		return vm;
 	}
-	public boolean getPaused(){
+
+	public boolean getPaused() {
 		return getVM().getValue("Jixel_paused");
 	}
-	public BufferStrategy getBuffer(){
+
+	public BufferStrategy getBuffer() {
 		return bs;
 	}
-	public synchronized JixelInput keys(){
+
+	public synchronized JixelInput getInput() {
 		return input;
 	}
 
