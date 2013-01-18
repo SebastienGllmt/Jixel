6a925561a46ba6026f53cc47fce5e1592f40d16c
SebastienGllmt <SebastienGllmt@gmail.com>
2013-01-17 22:02:49 -0800

    Now contains abstract render/update methods

--- src/stage/JixelGame.java
+++ src/stage/JixelGame.java
@@ -7,7 +7,7 @@ import console.JixelVariableManager;
 import entity.JixelEntityManager;
 import gui.JixelScreen;
 
-public class JixelGame {
+public abstract class JixelGame implements Runnable{
 
 	private static JixelVariableManager vm;
 	private static JixelConsole con;
@@ -20,6 +20,8 @@ public class JixelGame {
 	public boolean playing = true;
 	public final String GAME_TITLE;
 
+	Thread thread;
+	
 	public JixelGame(String title, int width, int height, int scale, int tileSize) {
 		GAME_TITLE = title;
 
@@ -33,6 +35,8 @@ public class JixelGame {
 		
 		input = new JixelInput();
 		screen.addKeyListener(input);
+		
+		start();
 	}
 
 	public String getTitle() {
@@ -59,12 +63,53 @@ public class JixelGame {
 		return input;
 	}
 	
-	public static JixelTimer getTimer(){
+	public JixelTimer getTimer(){
 		return timer;
 	}
 	
 	public static JixelEntityManager getEntityList(){
 		return entities;
+	}
+	
+	
+	/*			Abstract portion			*/
+	
+	public abstract void update();
+	public abstract void render();
+	
+	@Override
+	public void run(){
+		getTimer().startTimer(60, 60);
+		while (playing) {
+			while (!getPaused()) {
+				getTimer().updateTime();
+				if (getTimer().timeForUpdate() && !getConsole().isRunning()) {
+					getInput().updateKeyboard();
+					update();
+					getEntityList().update();
+				}
+				if (getTimer().timeForFrame()) {
+					getScreen().clear();
+					render();
+					getScreen().render();
+					getScreen().update();
+				}
+			}
+		}
+		stop();
+	}
+	
+	public synchronized void start() {
+		thread = new Thread(this, "GUI");
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
