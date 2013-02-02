6a9ca31b23692c7d269802bedfc964b558ebfa1e
SebastienGllmt <SebastienGllmt@gmail.com>
2013-02-02 03:18:35 -0800

    Thread-safe loading

--- src/stage/JixelGame.java
+++ src/stage/JixelGame.java
@@ -7,39 +7,42 @@ import console.JixelVariableManager;
 import entity.JixelEntityManager;
 import gui.JixelScreen;
 
-public abstract class JixelGame implements Runnable{
+public abstract class JixelGame implements Runnable {
 
 	private static JixelVariableManager vm;
 	private static JixelConsole con;
 	private static JixelScreen screen;
 	private static JixelInput input;
 	private static JixelTimer timer;
-	
+
 	private static JixelEntityManager entities = new JixelEntityManager();
 
 	public boolean playing = true;
 	public final String GAME_TITLE;
-	
+	private static boolean paused = false;
+
 	private final int fps, ups;
 
-	Thread thread; //thread for the update/render
-	
+	private final static Object updateLock = new Object();
+
+	Thread thread; // thread for the update/render
+
 	public JixelGame(String title, int width, int height, int scale, int tileSize, int fps, int ups) {
 		GAME_TITLE = title;
 		this.fps = fps;
 		this.ups = ups;
 
 		vm = new JixelVariableManager();
-		vm.newVar("Jixel_paused", false);
-		
+		getVM().newVar("Jixel_entityList", null);
+
 		screen = new JixelScreen(title, width, height, scale, tileSize);
 		timer = new JixelTimer(title);
-		
+
 		con = new JixelConsole();
-		
+
 		input = new JixelInput();
 		screen.addKeyListener(input);
-		
+
 		start();
 	}
 
@@ -59,46 +62,55 @@ public abstract class JixelGame implements Runnable{
 		return vm;
 	}
 
-	public boolean getPaused() {
-		return getVM().getValue("Jixel_paused");
+	public static boolean getPaused() {
+		return paused;
+	}
+
+	public static void setPaused(boolean newState) {
+		paused = newState;
 	}
 
 	public static synchronized JixelInput getInput() {
 		return input;
 	}
-	
-	public JixelTimer getTimer(){
+
+	public JixelTimer getTimer() {
 		return timer;
 	}
 
-	public static JixelEntityManager getEntityList(){
+	public static JixelEntityManager getEntityList() {
 		return entities;
 	}
-	
-	
-	public abstract void update(); //abstract update method for user to override
-	
+
+	public abstract void update();
+
+	public static Object getUpdateLock() {
+		return updateLock;
+	}
+
 	@Override
-	public void run(){
+	public void run() {
 		getTimer().startTimer(fps, ups);
 		while (playing) {
 			while (!getPaused()) {
-				getTimer().updateTime();
-				if (getTimer().timeForUpdate() && !getConsole().isRunning()) {
-					getInput().updateKeyboard();
-					update();
-					getEntityList().update();
-				}
-				if (getTimer().timeForFrame()) {
-					getScreen().clear();
-					getScreen().drawMap();
-					getScreen().drawEntities();
+				synchronized (getUpdateLock()) {
+					getTimer().updateTime();
+					if (getTimer().timeForUpdate() && !getConsole().isRunning()) {
+						getInput().updateKeyboard();
+						update();
+						getEntityList().update();
+					}
+					if (getTimer().timeForFrame()) {
+						getScreen().clear();
+						getScreen().drawMap();
+						getScreen().drawEntities();
+					}
 				}
 			}
 		}
 		stop();
 	}
-	
+
 	public synchronized void start() {
 		thread = new Thread(this, "GUI");
 		thread.start();
