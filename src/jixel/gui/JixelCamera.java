52a3e8dd416fe2addc08dba694b574f340a63cb0
Sebastien <SebastienGllmt@gmail.com>
2013-02-10 01:07:45 -0800

    No longer have to create a new camera just to change the size of the view

--- src/jixel/gui/JixelCamera.java
+++ src/jixel/gui/JixelCamera.java
@@ -20,18 +20,61 @@ public abstract class JixelCamera {
 	public abstract void drawOver(Graphics2D g);
 	
 	public abstract List<JixelEntity> getEntityList();
-	
-	public int getMinX(){
+
+	/**
+	 * @return the minX
+	 */
+	public int getMinX() {
 		return minX;
 	}
-	public int getMaxX(){
+
+	/**
+	 * @param minX the minX to set
+	 */
+	public void setMinX(int minX) {
+		this.minX = minX;
+	}
+
+	/**
+	 * @return the maxX
+	 */
+	public int getMaxX() {
 		return maxX;
 	}
-	public int getMinY(){
+
+	/**
+	 * @param maxX the maxX to set
+	 */
+	public void setMaxX(int maxX) {
+		this.maxX = maxX;
+	}
+
+	/**
+	 * @return the minY
+	 */
+	public int getMinY() {
 		return minY;
 	}
-	public int getMaxY(){
+
+	/**
+	 * @param minY the minY to set
+	 */
+	public void setMinY(int minY) {
+		this.minY = minY;
+	}
+
+	/**
+	 * @return the maxY
+	 */
+	public int getMaxY() {
 		return maxY;
+	}
+
+	/**
+	 * @param maxY the maxY to set
+	 */
+	public void setMaxY(int maxY) {
+		this.maxY = maxY;
 	}
 	
 }
